7569a07cca4942c8b53dc7d20094355a895c69d1
Stephanie Sorenson <stephaniesorenson@Stephanies-MacBook-Pro.local>
2018-01-24 14:26:05 -0500

    Updates for insight proj, including preproc within a function and combining cols eg if features are correlated

--- mlToolbox/pandas_helpers.py
+++ mlToolbox/pandas_helpers.py
@@ -4,12 +4,13 @@ import numpy as np
 import scipy as sp
 import pandas as pd
 
-print('v3')
+print('v5')
 
 # Preproc stuff
 ###########################
 from sklearn.utils import shuffle
-from sklearn.preprocessing import PolynomialFeatures, RobustScaler, Imputer
+from sklearn.preprocessing import PolynomialFeatures, StandardScaler, RobustScaler, Imputer
+from sklearn.feature_selection import SelectKBest, f_classif
 
 # Models
 ###########################
@@ -28,6 +29,9 @@ from sklearn.model_selection import KFold, StratifiedKFold
 ###########################
 from sklearn.metrics import mean_squared_error, median_absolute_error, explained_variance_score,\
                             roc_auc_score, f1_score, precision_score, recall_score
+
+from imblearn.over_sampling import SMOTE
+from imblearn.under_sampling import RandomUnderSampler
 
 ###############################################
 # Functions to create polynomial variables
@@ -194,11 +198,94 @@ def fillna_median(data, columns, grouping=False, val='median', verbose=True):
             print('Medians: ')
             print(meds)
 
+# do this to combine features in some way (eg averaging)
+def combine_feats(df, feat_list, new_name, agg_func='mean', drop_cols=True):
+    df = df.copy()
+    df[new_name] = df[feat_list].agg(agg_func, axis=1)
+    if drop_cols:
+        df.drop(feat_list, axis=1, inplace=True)
+    return df
+
+# Use this within CV folds (specify X_test), or optionally on the final dataset
+def preproc_data(X_train, y_train, resample_training,
+                 n_poly, scale_x, scale_cols,
+                 X_test=None, fsel=False, combine_cols=None):
+# give feat lists (dictionary of lists) to combine cols if you want to combine things
+
+    # Optionally add in interactions/n order polynomial features
+    # ****(right now this does to all -- need to fix!!!)***
+    if n_poly:
+        poly_feat = PolynomialFeatures(degree=n_poly, include_bias=False)
+        X_train = poly_feat.fit_transform(X_train)
+
+        if X_test is not None:
+            X_test = poly_feat.transform(X_test)
+
+    # Optionally scale features
+    if scale_x:
+        print('Scaling X...')
+        robust_scaler = StandardScaler()
+
+        # for numeric cols, fit model to training data
+        X_train.loc[:, scale_cols] = robust_scaler.fit_transform(X_train.loc[:, scale_cols])
+
+        # now update test if necessary
+        if X_test is not None:
+            X_test.loc[:, scale_cols] = robust_scaler.transform(X_test.loc[:, scale_cols])
+
+    # optionally combine similar features
+    if combine_cols is not None:
+        print(X_train.head())
+        for new_name in combine_cols.keys():
+            print('Creating ', new_name)
+            feat_list = combine_cols[new_name]
+            print(feat_list)
+            X_train = combine_feats(X_train, feat_list, new_name)
+
+            if X_test is not None:
+                X_test = combine_feats(X_test, feat_list, new_name)
+
+
+    # optionally upsample training
+    col_names = X_train.columns
+    if resample_training == 'over':
+        print('Upsampling with SMOTE...')
+        sm = SMOTE() # Synthetic Minority Over-sampling Technique
+        X_train, y_train = sm.fit_sample(X_train.as_matrix(), y_train.as_matrix())
+        X_train = pd.DataFrame(X_train, columns=col_names) # convert back to pandas
+
+    elif resample_training == 'under':
+        print('Undersampling randomly...')
+        rus = RandomUnderSampler()
+        X_train, y_train = rus.fit_sample(X_train.as_matrix(), y_train.as_matrix())
+        X_train = pd.DataFrame(X_train, columns=col_names) # convert back to pandas
+
+    if fsel:
+        selector = SelectKBest(f_classif, k=25)
+        selector.fit(X_train, y_train)
+        # Get idxs of columns to keep
+        idxs_selected = selector.get_support(indices=True)
+        # overwrite existing dataframe with only desired columns
+        X_train = X_train.iloc[:,idxs_selected]
+
+        if X_test is not None:
+            X_test = X_test.iloc[:,idxs_selected]
+
+    if X_test is not None:
+        # print(X_train.describe())
+        # print(X_test.describe())
+
+        return X_train, y_train, X_test
+    return X_train, y_train
+
+
 ###############################################
 # Model fitting
 ###############################################
 def fit_evaluate_models(X, y, dv_type, models, n_cv_folds=2,
-                        scale_x=False, n_poly=False, verbose=False):
+                        scale_x=False, n_poly=False, verbose=False,
+                        scale_cols=None, resample_training=None,
+                        estimate_feat_imp=True, fsel=False, combine_cols=None):
     ''' Fit and evaluate models
     X: samples x features dataframe
     y: labels/DV values
@@ -206,6 +293,13 @@ def fit_evaluate_models(X, y, dv_type, models, n_cv_folds=2,
     models: dict, example: models = {'ols': ols, 'ridge': ridge, 'grad_boost': gboost}
     n_cv_folds: int, number of cross-validation folds
     '''
+
+    col_names = X.columns # grab these, in case functions convert pd df to nparray
+
+    # scale all the cols, if scale_cols not defined
+    if not scale_cols:
+        print('Scaling all columns! Otherwise, give some specific ones...')
+        scale_cols = list(X.columns)
 
     # Set up dataframe to store output + CV scheme
     # Is the DV numeric or categorical?
@@ -232,20 +326,18 @@ def fit_evaluate_models(X, y, dv_type, models, n_cv_folds=2,
         y_train = y[train]
         y_test = y[test]
 
-        # Optionally add in interactions/n order polynomial features
-        if n_poly:
-            poly_feat = PolynomialFeatures(degree=n_poly, include_bias=False)
-            X_train = poly_feat.fit_transform(X_train)
-            X_test = poly_feat.fit_transform(X_test)
-
-        # Optionally scale features robustly
-        if scale_x:
-            robust_scaler = RobustScaler()
-            X_train = robust_scaler.fit_transform(X_train)
-            X_test = robust_scaler.transform(X_test)
+        # Proc data
+        X_train, y_train, X_test = preproc_data(X_train, y_train,
+                                                resample_training,
+                                                n_poly, scale_x, scale_cols,
+                                                X_test=X_test, fsel=fsel,
+                                                combine_cols=combine_cols)
 
         # Evaluate
         for model_name, model in models.items():
+
+            if verbose:
+                print(model_name)
 
             # Fit the model
             model.fit(X_train, y_train)
@@ -275,7 +367,7 @@ def fit_evaluate_models(X, y, dv_type, models, n_cv_folds=2,
 
                     # Binary classification
                     if len(y.unique()) == 2:
-                        if model_name not in ['random forest', 'knn (5)', 'dummy']:
+                        if model_name not in ['random forest', 'knn', 'dummy']:
                             y_score = model.decision_function(xs)
 
                             row = {'model': model_name,
@@ -285,6 +377,7 @@ def fit_evaluate_models(X, y, dv_type, models, n_cv_folds=2,
                                    'f1': f1_score(ys, model.predict(xs)),
                                    'precision': precision_score(ys, model.predict(xs)),
                                    'recall': recall_score(ys, model.predict(xs))}
+
                         else:
                             row = {'model': model_name,
                                    'eval_type': eval_type,
@@ -293,6 +386,7 @@ def fit_evaluate_models(X, y, dv_type, models, n_cv_folds=2,
                                    'f1': f1_score(ys, model.predict(xs)),
                                    'precision': precision_score(ys, model.predict(xs)),
                                    'recall': recall_score(ys, model.predict(xs))}
+
                     # >2 class classification
                     else:
                         row = {'model': model_name,
@@ -307,4 +401,34 @@ def fit_evaluate_models(X, y, dv_type, models, n_cv_folds=2,
                 # Append to dataframe
                 df_eval = df_eval.append(row, ignore_index=True)
 
+    # Optionally get the feature importances for the model, fit to all data
+    if estimate_feat_imp:
+
+        # preproc the data
+        X, y = preproc_data(X, y, resample_training,
+                            n_poly, scale_x, scale_cols, fsel=fsel,
+                            combine_cols=combine_cols)
+
+        print('Initializing storage for features')
+        col_names = X.columns
+        feat_imp = pd.DataFrame(columns=['model'] + list(col_names))
+
+        for model_name, model in models.items():
+            if model_name == 'logreg':
+                print('Fitting '); print(model)
+                model.fit(X, y)
+
+                feat_imp = feat_imp.append(pd.Series([model_name] + list(model.coef_[0]),
+                                                     index=['model'] + list(col_names)),
+                                           ignore_index=True)
+            elif model_name in ['grad_boost_class', 'random forest']:
+                print('Fitting '); print(model)
+                model.fit(X, y)
+
+                feat_imp = feat_imp.append(pd.Series([model_name] + list(model.feature_importances_),
+                                                     index=['model'] + list(col_names)),
+                                           ignore_index=True)
+
+    if estimate_feat_imp:
+        return df_eval, feat_imp
     return df_eval
